68114c11c51b1fb575c15d455b98bd4d764c1865
mendezvaleria716-cpu <mendezvaleria716@gmail.com>
2026-05-23 23:02:37 -0500

    Formulario

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 from pathlib import Path
 from fastapi import FastAPI, Form, Request
 from fastapi.responses import HTMLResponse, RedirectResponse
-from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from database import get_user, verify_password
 
 BASE_DIR = Path(__file__).resolve().parent
 
 app = FastAPI(title="ATLAS")
-app.mount("/static", StaticFiles(directory=BASE_DIR / "static"), name="static")
 templates = Jinja2Templates(directory=BASE_DIR / "templates")
 
 @app.get("/", response_class=HTMLResponse)
